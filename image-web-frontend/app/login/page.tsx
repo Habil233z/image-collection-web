bbadc746d45e37e59f7c288a6cae5f550523bb68
habil <habilherdianto233z@gmail.com>
2026-09-21 20:49:03 +0700

    remove autocomplete on login

--- image-web-frontend/app/login/page.tsx
+++ image-web-frontend/app/login/page.tsx
@@ -25,7 +25,7 @@ export default function Login() {
             <h1 className="text-center text-black mb-5 text-5xl">Login Page</h1>
             <div className="h-100 w-150 bg-gray-50 flex flex-col">
                 <form className="flex flex-col items-center justify-center w-full h-full mt-5">
-                    <input className="text-black" type="text" placeholder="Email or Username" minLength={5} required onChange={(e) => setEmailOrUsername(e.target.value)} value={emailOrUsername}/>
+                    <input className="text-black" type="text" placeholder="Email or Username" minLength={5} required onChange={(e) => setEmailOrUsername(e.target.value)} value={emailOrUsername} autoComplete="off"/>
                     <input className="text-black mt-2" type="password" placeholder="Password" minLength={8} required onChange={(e) => setPassword(e.target.value)} value={password}/>
                     <button className="text-black bg-gray-500 mt-4 px-3 py-2" onClick={e => {clickLogin(e)}}>Login</button>
                 </form>
