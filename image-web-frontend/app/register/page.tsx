fb3e9b8c0f6b83548fcece56741a0e22b366395f
habil <habilherdianto233z@gmail.com>
2026-09-21 20:55:05 +0700

    connect register frontend to backend

--- image-web-frontend/app/register/page.tsx
+++ image-web-frontend/app/register/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import api from "@/lib/api"
 import Link from "next/dist/client/link"
 import { useRouter } from "next/navigation"
 import { useState } from "react"
@@ -11,12 +12,19 @@ export default function Register() {
 
     const router = useRouter()
 
-    function clickRegister() {
+    async function clickRegister(e: any) {
+        e.preventDefault()
         console.log(email, username, password)
         if (!email.match(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/i) || username.length <= 5 || password.length <= 8) {
             return alert("One of criteria is not fulfilled")
         } else {
-            router.push("/login")
+            try {
+                await api.post("http://localhost:8080/register", {email, username, password})
+                console.log("Register successfull")
+                router.push("/login")
+            } catch {
+                console.log("Register failed")
+            }
         }
     }
 
@@ -24,11 +32,11 @@ export default function Register() {
         <div className="w-screen h-screen bg-gray-300 flex flex-col items-center justify-center">
             <h1 className="text-center text-black mb-5 text-5xl">Register Page</h1>
             <div className="h-100 w-150 bg-gray-50 flex flex-col">
-                <form className="flex flex-col items-center justify-center w-full h-full mt-5">
+                <form className="flex flex-col items-center justify-center w-full h-full mt-5" autoComplete="off">
                     <input className="text-black" type="email" placeholder="Email" required value={email} onChange={(e) => setEmail(e.target.value)}/>
                     <input className="text-black" type="text" placeholder="Username" minLength={5} required value={username} onChange={(e) => setUsername(e.target.value)}/>
                     <input className="text-black mt-2" type="password" placeholder="Password" minLength={8} required value={password} onChange={(e) => setPassword(e.target.value)}/>
-                    <button type="submit" onClick={clickRegister} className="text-black bg-gray-500 mt-4 px-3 py-2">Register</button>
+                    <button type="submit" onClick={e => {clickRegister(e)}} className="text-black bg-gray-500 mt-4 px-3 py-2">Register</button>
                 </form>
                 <Link href="/login" className="bg-gray-200 text-black mt-4 px-3 py-4">Login</Link>
             </div>
